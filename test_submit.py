730e66c35e4d97b34a04fd679987e54e43ef2187
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-04-22 18:48:23 +0800

    update

--- test_submit.py
+++ test_submit.py
@@ -5,10 +5,11 @@ from pyspark.sql import SparkSession
 DATA_DIR = "./data/population-data"
 # DATA_DIR = "./data/chronic-disease-data"
 
+# .\data\population-data\Alabama_pop_by_sex_and_age_2000-2010.xls
 path = os.path.join(DATA_DIR, "Alabama_pop_by_sex_and_age_2000-2010.xls")
 # path = os.path.join(DATA_DIR, "U.S._Chronic_Disease_Indicators__CDI___2023_Release.csv")
 
-spark = SparkSession.builder.appName('feature-engineering')\
+spark = SparkSession.builder.appName('test')\
     .config("spark.jars.packages", "com.crealytics:spark-excel_2.12:3.5.1_0.20.4")\
     .getOrCreate()
 
@@ -23,3 +24,4 @@ test_spark_df_00_10 = spark.read.format("com.crealytics.spark.excel")\
 #     .load(path)
 
 test_spark_df_00_10.show()
+
