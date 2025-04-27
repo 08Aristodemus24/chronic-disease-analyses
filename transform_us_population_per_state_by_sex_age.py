bbc861ab304b72e3be3fe2a5f7353d6fb8179129
08Aristodemus24 <MichaelAveuc571@gmail.com>
2025-04-27 16:18:58 +0800

    update

--- transform_us_population_per_state_by_sex_age.py
+++ transform_us_population_per_state_by_sex_age.py
@@ -22,36 +22,16 @@ from pyspark.sql.dataframe import DataFrame
 
 from concurrent.futures import ThreadPoolExecutor
 
-# def get_state_populations(DATA_DIR: str, cols_to_remove: list, populations: list, year_range: str, by: str) -> pd.DataFrame:
-#     def concur_model_pop_tables(file, to_remove, year_range, callback_fn=model_population_table):
-#         FILE_PATH = os.path.join(DATA_DIR, file)
-#         state = re.search(r"(^[A-Za-z\s]+)", file)
-#         state = "Unknown" if not state else state[0]
-
-#         # print(to_remove)
-#         # print(year_range)
-#         # read excel file
-#         df = pd.read_excel(FILE_PATH, dtype=object, header=None)
-        
-#         state_population = callback_fn(df, state, to_remove, year_range=year_range)
-#         return state_population
-    
-#     with ThreadPoolExecutor() as exe:
-#         callback_fn = model_population_by_sex_race_ho_table if "sex race and ho" in by else model_population_table
-#         state_populations = list(exe.map(
-#             concur_model_pop_tables, 
-#             populations, 
-#             [cols_to_remove] * len(populations),
-#             [year_range] * len(populations),
-#             [callback_fn] * len(populations)
-#         ))
-
-#     state_populations_df = pd.concat(state_populations, axis=0, ignore_index=True)
-
-#     return state_populations_df
 
 
-def process_population_by_sex_age_table(df: DataFrame, state: str, cols_to_remove: list, year_range: str="2000-2009"):
+def process_population_by_sex_age_table(df: DataFrame, 
+    state: str, 
+    cols_to_remove: list, 
+    year_range: str="2000-2009"):
+    """
+    process a loaded spark dataframe
+    """
+
     # extract numbers from year range
     years = re.findall(r"\d+", year_range)
     lo_year = ast.literal_eval(years[0])
@@ -311,13 +291,27 @@ def process_population_by_sex_age_table(df: DataFrame, state: str, cols_to_remov
     # concatenate final population brackets using
     # unionByName as callback
     final = reduce(DataFrame.unionByName, pop_brackets_final)
-    final.show()
 
+    # return spark dataframe
     return final
 
 
 
-def get_state_populations(DATA_DIR: str, cols_to_remove: list, populations: list, year_range: str, by: str):
+def get_state_populations_by_sex_age(
+    DATA_DIR: str,
+    session: SparkSession,
+    cols_to_remove: list,
+    populations: list,
+    year_range: str,
+    by: str):
+    
+    """
+    concurrently runs the state population processor functions for
+    each dataframe loaded
+    """
+
+    # define function to read each excel file as spark
+    # dataframe concurrently
     def concur_model_pop_tables(file, to_remove, year_range, callback_fn=process_population_by_sex_age_table):
         FILE_PATH = os.path.join(DATA_DIR, file)
         state = re.search(r"(^[A-Za-z\s]+)", file)
@@ -326,7 +320,7 @@ def get_state_populations(DATA_DIR: str, cols_to_remove: list, populations: list
         # print(to_remove)
         # print(year_range)
         # read excel file
-        df = spark.read.format("com.crealytics.spark.excel")\
+        df = session.read.format("com.crealytics.spark.excel")\
         .option("header", "false")\
         .option("inferSchema", "true")\
         .load(FILE_PATH)
@@ -334,7 +328,8 @@ def get_state_populations(DATA_DIR: str, cols_to_remove: list, populations: list
         state_population = callback_fn(df, state, to_remove, year_range=year_range)
         return state_population
     
-    with ThreadPoolExecutor() as exe:
+    # concurrently run function to build spark dataframes
+    with ThreadPoolExecutor(max_workers=2) as exe:
         callback_fn = function if "sex race and ho" in by else process_population_by_sex_age_table
         state_populations = list(exe.map(
             concur_model_pop_tables, 
@@ -344,7 +339,8 @@ def get_state_populations(DATA_DIR: str, cols_to_remove: list, populations: list
             [callback_fn] * len(populations)
         ))
 
-    state_populations_df = pd.concat(state_populations, axis=0, ignore_index=True)
+    # concatenate and unionize all spark dataframes
+    state_populations_df = reduce(DataFrame.unionByName, state_populations)
 
     return state_populations_df
 
@@ -360,42 +356,47 @@ if __name__ == "__main__":
     populations_by_sex_age_20_23 = list(filter(lambda file: "2020-2023" in file and "by_sex_and_age" in file, files))
     populations_by_sex_race_ho_20_23 = list(filter(lambda file: "2020-2023" in file and "by_sex_race_and_ho" in file, files))
 
-    # sys.argv
-    year_range = sys.argv[1]
-    state = sys.argv[2]
-
-    # .\data\population-data\Alabama_pop_by_sex_and_age_2000-2010.xls
-    FILE = f"{state}_pop_by_sex_and_age_{"2000-2010" if year_range == "2000-2009" else year_range}.xls{"" if year_range == "2000-2009" else "x"}"
-    FILE_PATH = os.path.join(DATA_DIR, FILE)
-    # path = os.path.join(DATA_DIR, "U.S._Chronic_Disease_Indicators__CDI___2023_Release.csv")
-
+    # create spark session
     spark = SparkSession.builder.appName('test')\
         .config("spark.jars.packages", "com.crealytics:spark-excel_2.12:3.5.1_0.20.4")\
         .getOrCreate()
 
-    df = spark.read.format("com.crealytics.spark.excel")\
-        .option("header", "false")\
-        .option("inferSchema", "true")\
-        .load(FILE_PATH)
+    # get year range from system arguments sys.argv
+    state_populations_all_years = []
+    year_ranges = sys.argv
 
-    # 2010 - 2019
-    if year_range == "2000-2009":
-        cols_to_remove = [1, 12, 13]
-    elif year_range == "2010-2019":
-        cols_to_remove = [1, 2, 3, 4, 5, 6] + list(range(7, len(df.columns), 3))
-    elif year_range == "2020-2023":
-        cols_to_remove = [1, 2, 3, 4] + list(range(7, len(df.columns), 3))
-    
-    process_population_by_sex_age_table(df, state, cols_to_remove=cols_to_remove, year_range=year_range)
+    # loop through year_ranges
+    for year_range in year_ranges[1:]:
+        # 2000 - 2010
+        if year_range == "2000-2009":
+            cols_to_remove = [1, 12, 13]
+            populations = populations_by_sex_age_00_10 
 
+        # 2010 - 2019
+        elif year_range == "2010-2019":
+            cols_to_remove = [1, 2, 3, 4, 5, 6, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+            populations = populations_by_sex_age_10_19
 
-    # start_index = df[df[0] == ".0"].index.to_list()[0]
-    # end_index = df[df[0] == ".Median Age (years)"].index.to_list()[0]
+        # 2020 - 2023
+        elif year_range == "2020-2023":
+            cols_to_remove = [1, 2, 3, 4, 7, 10, 13]
+            populations = populations_by_sex_age_20_23
 
-    # # extract necessary rows
-    # pop_brackets_raw = df.iloc[start_index: end_index]
-    
-    # # remove duplicatess
-    # temp = pop_brackets_raw.drop_duplicates()
+        # concurrently process state populations by year range
+        state_populations_df = get_state_populations_by_sex_age(
+            DATA_DIR, 
+            spark, 
+            cols_to_remove, 
+            populations, 
+            year_range, 
+            by="sex and age")
+        
+        # collect state populations from all years using list
+        state_populations_all_years.append(state_populations_df)
 
+    # concatenate all state populations from all year ranges
+    final = reduce(DataFrame.unionByName, state_populations_all_years)
+    final.show()
+
+    print(f"final population count: {final.count()}")
     
